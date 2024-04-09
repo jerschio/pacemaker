37163ab4a41de4f202dfd23b19de0c5d2a9bb216
Jeroen Schiøtt <jeroen.schiott@gmail.com>
2024-04-09 20:21:59 +0200

    urlToUse

--- src/routes/Homelink.svelte
+++ src/routes/Homelink.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import { base } from '$app/paths';
 
+	const urlToUse = base ? '/pacemaker' : '/';
+
 	console.log('base:', base);
 </script>
 
 <div class="homelink">
-	<a href="/pacemaker">Home</a>
+	<!-- <a href="/pacemaker">Home</a> -->
+	<a href={urlToUse}>Home</a>
 </div>
 
 <style>
